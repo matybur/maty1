98adb99a0c5d3173a84ae604cbab0e57e6a37ac8
matybur <167638589+matybur@users.noreply.github.com>
2024-05-20 18:35:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+basic.showLeds(`
+    # # # # #
+    # # . # #
+    # . . . #
+    # # . # #
+    # # # # #
+    `)
+basic.pause(2000)
+basic.showIcon(IconNames.TShirt)
+basic.pause(2000)
+basic.showString("Hello!")
 basic.forever(function () {
 	
 })
